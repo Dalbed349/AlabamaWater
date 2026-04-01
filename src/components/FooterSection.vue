80987570c6bdc3864f4bc88cdac62df66d532022
Dalbed349 <69644845+Dalbed349@users.noreply.github.com>
2026-04-01 02:22:53 -0400

    refactor: restore methodology section and clean up footer aesthetics

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,52 +1,48 @@
 <template>
-  <div class="map">
-    <div class="curved-div">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#fff"
-          fill-opacity="1"
-          d="M0,96L80,80C160,64,320,32,480,48C640,64,800,128,960,133.3C1120,139,1280,85,1360,58.7L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-        ></path>
-      </svg>
-    </div>
-    <h1>What is being done?</h1>
-    <img src="../assets/keepers.svg" />
-
-    <div class="transbox">
+  <footer class="FooterSection">
+    <div class="content-container">
       <h2>The Riverkeepers</h2>
-      <p>
+      <p class="footer-intro">
         Various organizations help study and protect the water basins listed in
-        this dataset.
-        <br /><br />They act independently, but help inform decisions, from
-        state agencies such as Alabama Department of Environmental Protection.
-        <br />Current news and research regarding each basin linked below or
-        visit: <a href="http://waterkeepersalabama.org/" target="_blank">http://waterkeepersalabama.org/</a>
-        <br />
-        <br />
+        this dataset. They act independently but inform decisions by state 
+        agencies such as the Alabama Department of Environmental Management (ADEM).
       </p>
-      <p id="resourcelinks">
-        <a href="https://www.mobilebaykeeper.org/" target="_blank">https://www.mobilebaykeeper.org/</a><br />
-        <a href="https://blackwarriorriver.org/" target="_blank">https://blackwarriorriver.org/</a><br />
-        <a href="https://www.tennesseeriverkeeper.org/" target="_blank">https://www.tennesseeriverkeeper.org/</a><br />
-        <a href="https://littleriverwaterkeeper.org/" target="_blank">https://littleriverwaterkeeper.org/</a><br />
-        <a href="https://coosa.org/" target="_blank">https://coosa.org/</a><br />
-        <a href="https://chattahoochee.org/" target="_blank">https://chattahoochee.org/</a><br />
-        <a href="https://cahabariverkeeper.org/" target="_blank">https://cahabariverkeeper.org/</a><br />
-        <a href="https://www.choctawhatcheeriver.org/" target="_blank">https://www.choctawhatcheeriver.org/</a><br />
-        <a href="http://www.hurricanecreek.org/" target="_blank">http://www.hurricanecreek.org/</a><br />
-      </p>
+
+      <div class="resource-grid">
+        <div class="riverkeepers-list">
+          <h3>Local Organizations</h3>
+          <p id="resourcelinks">
+            <a href="https://www.mobilebaykeeper.org/" target="_blank">Mobile Baykeeper</a><br />
+            <a href="https://blackwarriorriver.org/" target="_blank">Black Warrior Riverkeeper</a><br />
+            <a href="https://www.tennesseeriverkeeper.org/" target="_blank">Tennessee Riverkeeper</a><br />
+            <a href="https://littleriverwaterkeeper.org/" target="_blank">Little River Waterkeeper</a><br />
+            <a href="https://coosa.org/" target="_blank">Coosa Riverkeeper</a><br />
+            <a href="https://chattahoochee.org/" target="_blank">Chattahoochee Riverkeeper</a><br />
+            <a href="https://cahabariverkeeper.org/" target="_blank">Cahaba Riverkeeper</a><br />
+            <a href="https://www.choctawhatcheeriver.org/" target="_blank">Choctawhatchee Riverkeeper</a><br />
+            <a href="http://www.hurricanecreek.org/" target="_blank">Friends of Hurricane Creek</a>
+          </p>
+          <p class="central-link">
+            Visit: <a href="http://waterkeepersalabama.org/" target="_blank">Waterkeepers Alabama</a>
+          </p>
+        </div>
+
+        <div class="project-info">
+          <h3>About the Project</h3>
+          <p>
+            This project was prepared as part of the Parsons Data Visualization
+            (M.S.) Program.
+          </p>
+          <p>
+            Explore the source code and documentation at: <br>
+            <a href="https://github.com/Dalbed349/AlabamaWater/" target="_blank">
+              github.com/Dalbed349/AlabamaWater/
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-    <div class="transbox2">
-      <p>
-        This project was prepared as part of the Parson's Data Visualization
-        (M.S.) Program. <br /><br />
-        Files and documentation can be found at
-        <a href="https://github.com/Dalbed349/AlabamaWater/"
-          >github.com/Dalbed349/AlabamaWater/</a
-        >
-      </p>
-    </div>
-  </div>
+  </footer>
 </template>
 
 <script>
@@ -56,121 +52,62 @@ export default {
 </script>
 
 <style scoped>
-.map {
+.FooterSection {
+  padding: 100px 0;
+  background-color: #1a1a1a;
+  color: #fff;
+}
+h2 {
+  font-size: 3rem;
+  margin-bottom: 20px;
+}
+.footer-intro {
+  font-size: 1.2rem;
+  color: #ccc;
+  margin-bottom: 60px;
+  max-width: 600px;
+}
+.resource-grid {
   display: grid;
-  position: relative;
-  z-index: -1;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: linear-gradient(
-      to right,
-      rgba(255, 255, 255, 0),
-      rgba(59, 45, 4, 0.198)
-    ),
-    linear-gradient(to left, rgba(255, 255, 255, 0), rgba(23, 99, 143, 0.616)),
-    url("../assets/cahaba.jpg");
-  background-repeat: no-repeat;
-  width: 100%;
-  height: 100vh;
-  opacity: 1;
-  margin-top: 7%;
-  grid-template-columns: 40px 50px auto 50px 40px;
-  grid-template-rows: 25% 100px 100px 100px auto;
-  margin-bottom: -1%;
-  background-size: 100% 100%;
+  grid-template-columns: 1fr 1fr;
+  gap: 80px;
+  text-align: left;
 }
-.map .curved-div {
-  margin-top: -3%;
-  position: relative;
-  grid-column-start: 1;
-  grid-column-end: 6;
-  grid-row-start: 1;
-  grid-row-end: 1;
+h3 {
+  font-size: 1.5rem;
+  margin-bottom: 25px;
+  color: teal;
 }
-.curved-div {
-  position: block;
-  height: 55vh;
-  width: 100%;
+#resourcelinks a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.1rem;
+  line-height: 2;
+  transition: color 0.2s;
 }
-.curved-div svg {
-  width: 100%;
-  height: auto;
-  text-align: top;
-  margin-top: 2%;
+#resourcelinks a:hover {
+  color: teal;
 }
-.map h1 {
-  margin-right: 0%;
-  margin-top: 1%;
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: span 2;
-  font-size: 3em;
-  z-index: 1;
-  color: #000;
-  margin-left: 45%;
-  margin-top: 3%;
+.central-link {
+  margin-top: 30px;
+  padding-top: 20px;
+  border-top: 1px solid #333;
 }
-.map img {
-  transform: scale(9, 9);
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 2;
-  border-radius: 20px;
-  margin-top: -5%;
-  height: 100%;
-  margin-left: 35%;
-  margin-top: 13%;
-}
-.map .transbox {
-  margin-left: 55%;
-  margin-right: 5%;
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: span 2;
-  font-size: 1em;
-  color: #000000;
-  margin-bottom: -20.5%;
-  margin-top: 1%;
-  border-radius: 50px;
-  background-color: #ffffff;
-  border: none;
+.central-link a {
+  color: teal;
   font-weight: bold;
 }
-.map .transbox h2 {
-  margin-top: 2%;
-  margin-bottom: -2%;
-  margin-left: 5%;
-  margin-right: 5%;
+.project-info p {
+  color: #aaa;
+  margin-bottom: 20px;
 }
-.map .transbox p {
-  margin-top: 5%;
-  font-size: 1.1em;
-  margin-left: 5%;
-  margin-right: 5%;
+.project-info a {
+  color: teal;
 }
-.map .transbox2 {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 5;
-  grid-row-end: 6;
-  border-radius: 55px;
-  background-color: #ffffff;
-  opacity: 1; /* Removed duplicate 0.9 */
-  margin-bottom: 10%;
-  margin-top: 17%;
-  margin-left: 53%;
-  margin-right: 5%;
-}
-.map .transbox2 p {
-  margin-top: 2%;
-  font-size: 1em;
-}
-#resourcelinks {
-  font-size: 0.9em;
-  margin-top: 0%;
+@media (max-width: 768px) {
+  .resource-grid {
+    grid-template-columns: 1fr;
+    gap: 40px;
+  }
 }
 </style>
--- src/components/MapBasinSection.vue
+++ src/components/MapBasinSection.vue
@@ -1,43 +1,40 @@
 <template>
-  <div class="map2">
-    <h1>The River Basins of Alabama</h1>
-    <img alt="Vue" src="../assets/riverBasins.png" />
-    <div class="transbox2">
-      <p>
-        Since 1998 stricter water monitoring policies have been mandated in the
+  <section class="MethodologySection">
+    <div class="content-container">
+      <h2>Data & Methodology</h2>
+      <p class="intro-text">
+        Since 1998, stricter water monitoring policies have been mandated in the
         state in an attempt to control the human and environmental harm caused
         by pollutants.
       </p>
-      <p>
-        Section 303(d) of the Clean Water Act (CWA) establishes that states are
-        to list (the 303(d) list) waters for which technology-based limits alone
-        do not ensure attainment of applicable water quality standards.
-        <br /><br /><br />
-        The Alabama Department of Environmental Management (ADEM) creates these
-        lists which can be found here:
-        <a href="http://adem.alabama.gov/programs/water/303d.cnt"
-          >ADEM 303(d)</a
-        >
-        <br /><br />
-        The past 20 years of impaired 303(d) waters are aggregated and organized
-        by the major river basin. Data is used to visualize the scope of water
-        contamination by size, cause, and source.This project seeks to analyze
-        the progress that has been made during the past 20 years of water
-        quality testing.
-      </p>
+
+      <div class="methodology-grid">
+        <div class="methodology-text">
+          <p>
+            Section 303(d) of the Clean Water Act (CWA) establishes that states are
+            to list waters for which technology-based limits alone do not ensure 
+            attainment of applicable water quality standards.
+          </p>
+          <p>
+            The Alabama Department of Environmental Management (ADEM) creates these
+            lists which can be found at the 
+            <a href="http://adem.alabama.gov/programs/water/303d.cnt" target="_blank">ADEM 303(d) Portal</a>.
+          </p>
+          <p class="highlight-box">
+            The past 20 years of impaired 303(d) waters are aggregated and organized
+            by the major river basin. This project seeks to analyze
+            the progress that has been made during these two decades of water
+            quality testing.
+          </p>
+        </div>
+        
+        <div class="methodology-image">
+          <img alt="River Basins of Alabama" src="../assets/riverBasins.png" />
+          <p class="caption">The River Basins of Alabama</p>
+        </div>
+      </div>
     </div>
-    <!--  -->
-    <div class="curved-div">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#fff"
-          fill-opacity="1"
-          d="M0,96L80,80C160,64,320,32,480,48C640,64,800,128,960,133.3C1120,139,1280,85,1360,58.7L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-        ></path>
-      </svg>
-      <div class="spacer"></div>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -47,92 +44,60 @@ export default {
 </script>
 
 <style scoped>
-.map2 {
+.MethodologySection {
+  padding: 100px 0;
+  background-color: #fff;
+}
+h2 {
+  font-size: 3rem;
+  margin-bottom: 40px;
+}
+.intro-text {
+  font-size: 1.4rem;
+  color: #444;
+  margin-bottom: 60px;
+  max-width: 700px;
+}
+.methodology-grid {
   display: grid;
-  position: relative;
-  z-index: -1;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin-top: 10%;
-  background: #f8fcfd;
+  grid-template-columns: 1.2fr 0.8fr;
+  gap: 60px;
+  align-items: start;
+}
+.methodology-text p {
+  margin-bottom: 25px;
+  font-size: 1.15rem;
+  line-height: 1.7;
+}
+.highlight-box {
+  background-color: #f0f7f8;
+  padding: 30px;
+  border-radius: 8px;
+  border-left: 4px solid teal;
+  font-weight: 500;
+  color: #0d4d4d;
+}
+.methodology-image {
+  text-align: center;
+}
+.methodology-image img {
   width: 100%;
-  height: auto;
-  min-height: 100vh;
-  grid-template-columns: minmax(5px, 40px) minmax(5px, 50px) auto minmax(5px, 50px) minmax(5px, 40px);
-  grid-template-rows: 25% 100px 100px 100px auto;
+  max-width: 400px;
+  border-radius: 12px;
 }
-.map2 img {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 2;
-  grid-row-end: span 2;
-  margin-left: -2%;
-  border-radius: 20px;
+.caption {
+  font-size: 0.9rem;
+  color: #666;
+  margin-top: 15px;
+  font-style: italic;
 }
-.map2 h1 {
-  grid-column-start: 3;
-  grid-column-end: 4;
-  grid-row-start: 1;
-  grid-row-end: span 2;
-  z-index: 1;
-  font-size: clamp(2em, 5vw, 3em);
-  margin-left: 10%;
-  margin-top: 17%;
-}
-@media (min-width: 1024px) {
-  .map2 h1 {
-    margin-left: 35%;
+@media (max-width: 900px) {
+  .methodology-grid {
+    grid-template-columns: 1fr;
   }
-}
-.map2 .transbox2 {
-  margin-left: 5%;
-  margin-right: 5%;
-  grid-column-start: 3;
-  grid-column-end: 3;
-  grid-row-start: 3;
-  grid-row-end: 5;
-  border-radius: 55px;
-  opacity: 1;
-  margin-bottom: 7%;
-  margin-top: 7%;
-  text-align: left;
-}
-@media (min-width: 1024px) {
-  .map2 .transbox2 {
-    margin-left: 37%;
-    margin-right: 0;
+  .methodology-image {
+    order: -1;
+    margin-bottom: 40px;
   }
-}
-.map2 .transbox2 p {
-  margin-bottom: 4%;
-  text-align: left;
-  font-size: 1.3em;
-  opacity: 1;
-  font-weight: bold;
-}
-.map2 .curved-div {
-  grid-column-start: 1;
-  grid-column-end: 6;
-  grid-row-start: 1;
-  grid-row-end: 1;
-  margin-top: -6.1%;
-}
-.map2 .curved-div svg {
-  width: 100%;
-  height: auto;
-  text-align: top;
-  margin-top: 6%;
-}
-.curved-div {
-  position: block;
-  height: 55vh;
-  width: 100%;
-}
-.spacer {
-  display: block;
-  height: 2vh;
-  width: 100%;
 }
 </style>
